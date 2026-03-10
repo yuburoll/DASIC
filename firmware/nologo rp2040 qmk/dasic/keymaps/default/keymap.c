6aa083ab392eb76e4c96709afc66bf803066234c
yuburoll <yuburoll@gmail.com>
2026-03-10 20:25:41 +0900

    fix firmware and keymap error

--- firmware/nologo rp2040 qmk/dasic/keymaps/default/keymap.c
+++ firmware/nologo rp2040 qmk/dasic/keymaps/default/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
     [2] = LAYOUT_symmetrical_alice_64(
         KC_ESC,         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-                                                                KC_PSLS,        KC_PAST,        KC_PMNS,        KC_PPLS,        KC_NO,          KC_NO,          KC_BSPC,
+                                                                KC_PSLS,        KC_PAST,        KC_PMNS,        KC_NUM,         KC_NO,          KC_NO,          KC_BSPC,
 
         KC_NO,          KC_NO,          KC_UP,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
                                                                 KC_P7,          KC_P8,          KC_P9,          KC_PPLS,        KC_NO,          KC_DEL,         KC_NO,
